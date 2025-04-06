940f9450c01b502995601511cd011322c5566fda
Adrian Stachowski <adrianstachowski95@gmail.com>
2025-04-06 16:48:33 +0200

    built functions to get api and convert password to sha1 hash string

--- checkmypass.py
+++ checkmypass.py
@@ -1,5 +1,17 @@
 import requests
+import hashlib
 
-url = 'https://api.pwnedpasswords.com/range/' + 'CBFDA'     #First 5 symbols of SHA1 Hash string
-res = requests.get(url)
-print(res)
+
+def request_api_data(query_char):
+    url = 'https://api.pwnedpasswords.com/range/' + query_char  # First 5 symbols of SHA1 Hash string
+    res = requests.get(url)
+    if res.status != 200:
+        raise RuntimeError(f'Error fetching: {res.status_code}, check the API and try again')
+    return res
+
+def pwned_api_checl(password):
+    #Check password if it exists in API response
+    sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    return sha1password
+
+#request_api_data('password123')
